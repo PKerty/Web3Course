a210cf9e420e085fb2a95fd2f3e73fbaad60ad59
Pedro Curti <pcurti61@gmail.com>
2023-12-17 23:59:43 -0300

    started on solidity

---
+++ BasicStorage.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19; // stating solidity version
+
+//pragma solidity ^0.8.18 means it will work with the version stated and all the newers
+//in our case only will work with 0.8.18
+//Also can define ranges: pragma solidity >=0.8.18 <0.9.0
+
+
+contract SimpleStorage {
+    /*
+        Basic Types:
+            - boolean
+            - uint
+            - int
+            - address
+            - bytes
+            - string
+    */
+} 
