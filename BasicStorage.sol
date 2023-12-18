7f79b828a44b607a94ba6259667729a61c492efb
Pedro Curti <pcurti@itba.edu.ar>
2023-12-18 01:31:13 -0300

    iteration 3

--- BasicStorage.sol
+++ BasicStorage.sol
@@ -6,12 +6,15 @@ pragma solidity 0.8.19; // stating solidity version
 //Also can define ranges: pragma solidity >=0.8.18 <0.9.0
 
     /*
-        We can set up our favoriteNumber and read it since we changed the visibility
+        We can set up our favoriteNumber and read it using the new function and leaving default visibility
     */
 contract SimpleStorage {
     
-    uint256 public favoriteNumber256; //0 now visibility changed
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber256 = _favoriteNumber;
-    }
+        uint256 favoriteNumber256; //0 now visibility changed
+        function store(uint256 _favoriteNumber) public {
+            favoriteNumber256 = _favoriteNumber;
+        }
+        function retrieve() public view returns(uint256) {
+            retrun favoriteNumber256;
+        }
 }
